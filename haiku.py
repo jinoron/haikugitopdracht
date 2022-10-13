48eb224cf781af9661e731dfbfdc3bd57ded9491
jin oron <35735@ma-web.nl>
2022-10-13 14:48:09 +0200

    Haiku functies toegevoegd en eerste ingevuld

--- haiku.py
+++ haiku.py
@@ -1,6 +1,32 @@
 
 
 #plak hier de haiku functions onder elkaar
+haiku (## Haiku function template
+
+## Haiku function template
+
+def Haiku1():
+    print("haiku by:","Jin")
+    print("Consider me")
+    print("As one who loved poetry")
+    print("And persimmons.")
+
+## Haiku function template
+
+def Haiku2():
+    print("haiku by:","AUTHOR")
+    print("REGEL1")
+    print("REGEL2")
+    print("REGEL3")
+
+## Haiku function template
+
+def Haiku3():
+    print("haiku by:","AUTHOR")
+    print("REGEL1")
+    print("REGEL2")
+    print("REGEL3")
+
 
 
 
